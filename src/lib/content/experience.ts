99db723ce556dfbd61bf9e8afce8a2624af2e3c3
julius_mwangi <mwangimuringi10@gmail.com>
2024-06-07 18:57:41 +0300

    icons update

---
+++ src/lib/content/experience.ts
@@ -0,0 +1,45 @@
+import { ExperienceSectionType } from '@/lib/types/sections'
+
+export const experienceSection: ExperienceSectionType = {
+    title: "where i've worked",
+    experiences: [
+    {
+        company: 'TEKssystem',
+        companyUrl: 'https://www.teksystem.live/',
+        role: 'frontend developer and Java developer',
+        started: 'August 2022',
+        upto: 'present',
+        tasks: [
+        'Implemented UI components with React & Next.js, TypeScript, Javascript & Tailwind CSS.',
+        'Developed and maintained design systems that separates design logic.',
+        'Understanding client needs and proposing effective solutions, which also involves strategising and planning.',
+        'Lead a cross-functional team of developers and designers in the creation of a SaaS product.',
+        ],
+    },
+    {
+        company: 'JHUBAFRICA',
+        companyUrl: 'https://jhubafrica.com/',
+        role: 'frontend developer intern',
+        started: 'February 2024',
+        upto: 'August 2024',
+        tasks: [
+        'Built Kahawa Yetu (Coffee Cooperative application).',
+        'Designed Kahawa yetu website with Next.js, TypeScript & TailwindCSS.',
+        'Collaborated with diverse nationwide team of developers.',
+        ],
+    },
+    {
+        company: 'webnetics',
+        companyUrl: 'https://webnetic.vercel.app/',
+        role: 'frontend developer',
+        started: 'june 2022',
+        upto: 'january 2023',
+        tasks: [
+        'Built static frontend UI from the ground up using Next.js and Figma handover designs.',
+        'Architected the folder structure and initial setup of the app.',
+        'Reviewed and approved multiple Pull requests.',
+        'Worked remotely with a distributed team from around the globe, collaborating closely using screen-sharing.',
+        ],
+    },
+    ],
+};
---
+++ src/lib/content/footer.ts
@@ -0,0 +1,6 @@
+import { FooterSectionType } from '@/lib/types/sections';
+
+export const footerSection: FooterSectionType = {
+    title: 'Design & Julius Mwangi',
+    link: 'https://github.com/mwangi223/portfolio-website',
+};
